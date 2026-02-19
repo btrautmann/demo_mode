7865407b9af7516ba281493f8b0d5ed58aa686af
Brandon Trautmann <8343465+btrautmann@users.noreply.github.com>
2026-02-19 15:27:13 -0500

    flatten arguments

--- app/jobs/demo_mode/account_generation_job.rb
+++ app/jobs/demo_mode/account_generation_job.rb
@@ -2,7 +2,27 @@
 
 module DemoMode
   class AccountGenerationJob < DemoMode.base_job_name.constantize
+    attr_reader :session
+
+    # Called by Delayed::Plugins::LoggingPlugin to log job arguments.
+    # Returns a hash of arguments to be logged as structured data.
+    def loggable_arguments
+      return {} unless session
+
+      {
+        session_id: session.id,
+        persona_name: session.persona_name,
+        variant: session.variant&.to_s,
+      }
+    end
+
+    # Opt-in to flatten nested arguments for easier Datadog faceting.
+    def flatten_loggable_arguments?
+      true
+    end
+
     def perform(session, **options)
+      @session = session
       session.with_lock do
         persona = session.persona
         raise "Unknown persona: #{session.persona_name}" if persona.blank?
